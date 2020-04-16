b95bb763142766f18b9156053041f070b52f1ee6
William Diffey <50226488+williamdiffey@users.noreply.github.com>
2020-04-16 15:04:57 +0900

    restyling of hero

--- src/Hero.js
+++ src/Hero.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import WDicon from './images/WDicon.png'
 
 export default function Hero() {
   return (
     <header className='hero'>
       <h1 className='william'>WILLIAM</h1> <h1 className='diffey'>DIFFEY</h1>
+      {/* <img
+        alt='William Diffey Logo'
+        src={WDicon}
+        width='100'
+        height='100'
+        className='hero-logo'
+      ></img> */}
     </header>
   )
 }
